dacac820c32a06bdf40b786f9712406a03255d94
Nasri Adzlani <aliendev@Nasris-Mac-mini.lan>
2025-06-15 02:55:52 +0700

    hehe

--- embed.js
+++ embed.js
@@ -1,10 +1,11 @@
-(function() {
-  const linkCSS = document.createElement('link');
-  linkCSS.rel = 'stylesheet';
-  linkCSS.href = 'https://cdn.jsdelivr.net/gh/masnasri-a/chatbot-embed/chatbot.css';
-  document.head.appendChild(linkCSS);
-
-  const scriptBot = document.createElement('script');
-  scriptBot.src = 'https://cdn.jsdelivr.net/gh/masnasri-a/chatbot-embed/chatbot.js';
-  document.body.appendChild(scriptBot);
+(function () {
+    const linkCSS = document.createElement('link');
+    linkCSS.rel = 'stylesheet';
+    linkCSS.href = 'https://cdn.jsdelivr.net/gh/masnasri-a/chatbot-embed/chatbot.css';
+    document.head.appendChild(linkCSS);
+    console.log('Chatbot CSS loaded');
+    const scriptBot = document.createElement('script');
+    scriptBot.src = 'https://cdn.jsdelivr.net/gh/masnasri-a/chatbot-embed/chatbot.js';
+    document.body.appendChild(scriptBot);
+    console.log('Chatbot script loaded');
 })();
