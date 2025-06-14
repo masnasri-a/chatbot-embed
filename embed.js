1510573f8a61b926bffd379052e02e7ba54885a4
Nasri Adzlani <aliendev@Nasris-Mac-mini.lan>
2025-06-15 02:46:31 +0700

    hehe

--- embed.js
+++ embed.js
@@ -1,10 +1,10 @@
 (function() {
   const linkCSS = document.createElement('link');
   linkCSS.rel = 'stylesheet';
-  linkCSS.href = 'https://yourdomain.com/chatbot.css';
+  linkCSS.href = 'https://raw.githubusercontent.com/masnasri-a/chatbot-embed/refs/heads/main/chatbot.css';
   document.head.appendChild(linkCSS);
 
   const scriptBot = document.createElement('script');
-  scriptBot.src = 'https://yourdomain.com/chatbot.js';
+  scriptBot.src = 'https://raw.githubusercontent.com/masnasri-a/chatbot-embed/refs/heads/main/chatbot.js';
   document.body.appendChild(scriptBot);
 })();
